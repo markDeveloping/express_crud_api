4eeee8ec012854791ab2895ec6a3b721fa30eb8d
markDeveloping <mark.developing@gmail.com>
2018-05-16 21:12:52 +0100

    Setup mLab connection and was able to make make post requests to the database.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
+const db             = require('./config/db');
 
 const app            = express();
 
@@ -10,7 +11,12 @@ const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('./app/routes')(app, {});
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
+
+
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });               
+})
