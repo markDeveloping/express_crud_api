03436299ec8d361205ded22c41ec32954d5b6a97
markDeveloping <mark.developing@gmail.com>
2018-05-17 21:04:41 +0100

    Added get single record endpoint.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,20 @@
 
 module.exports = function(app, db) {
-  const collection = 
+
+	var ObjectID = require('mongodb').ObjectID;
+
+	app.get('/notes/:id', (req, res) => {
+		const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    });
+      });
+  
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
     db.collection('notes').insert(note, (err, result) => {
